6e56458cc3327d54e81440eb204dd06e2803686a
César Agustín Ozuna <agustinozuna@hotmail.com>
2020-12-02 19:00:50 -0300

    Continuación de codificacion de contralador y vista de la entidad Mantenimiento

--- SistemaControlDeTrabajo/Controllers/MantenimientoController.cs
+++ SistemaControlDeTrabajo/Controllers/MantenimientoController.cs
@@ -20,7 +20,40 @@ namespace SistemaControlDeTrabajo.Controllers
 
         public ActionResult Crear()
         {
+
+
+      
+
+
+            var listaContrato = db.Contrato.ToList();
+          //  ViewData["contratos"] = listaContrato.AsEnumerable();
+
+            var selectListContrato = new SelectList(listaContrato, "CodContrato", "NroContrato", 2);
+            ViewData["contratos2"] = selectListContrato;
+
+
+            var listaEmpleados = db.Personal.ToList();
+            //ViewData["empleados"] = listaEmpleados.AsEnumerable();
+
+
+            var selectListPersonal = new SelectList(listaEmpleados, "CodPersonal", "NombreApellido", 2);
+            ViewData["empleados2"] = selectListPersonal;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Mantenimiento mant)
+        {
+            if (ModelState.IsValid)
+            {
+                mant.FechaRealizacion = DateTime.Now;
+                db.Mantenimiento.Add(mant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mant);
         }
     }
 }
